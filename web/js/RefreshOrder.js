179ea6223ef683d03e116fa7f4851b191780c878
TR <1422397490@qq.com>
2019-07-05 09:15:47 +0800

    Merge branch 'master' of D:\Java程序\Kmust-shortTeam-six with conflicts.

--- web/js/RefreshOrder.js
+++ web/js/RefreshOrder.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
                         dataType: "json",
                         success : function (data1) {
                             image = data1[0].imagePath;
-                            html = html + "</tr>";
+                            html = html + "<tr>";
                             html = html + "<td><a href=\"\">"+ orderId +"</a></td>";
                             html = html + "<td><a href=\"\"><img src=\"images/"+ image +"\" alt=\"\" /></a></td>";
                             html = html + "<td>" + userId + "</td>";
@@ -41,7 +41,7 @@ $(document).ready(function () {
                             html = html + "<td>" + "已成功" + "</td>";
                             html = html + "<td><a href=\"\">"+ "查看" + "</a> | <a href=\"\">" + "删除" + "</a></td>";
                             html = html + "</tr>";
-                            //alert(html)
+                            alert(html)
                             $("#datalist").html(html);
                         },
                         error : function () {
